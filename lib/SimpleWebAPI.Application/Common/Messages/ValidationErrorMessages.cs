d81b2dd4fe3e7e6ab0770f7bf18598bf983e42a5
Patrick Lamm <lammster@gmail.com>
2023-03-23 16:19:04 -0400

    updated error messages

--- lib/SimpleWebAPI.Application/Common/Messages/ValidationErrorMessages.cs
+++ lib/SimpleWebAPI.Application/Common/Messages/ValidationErrorMessages.cs
@@ -2,11 +2,11 @@
 {
     public static class ValidationErrorMessages
     {
-        public static string ERROR_NAME_EXISTS = "Error: Name already exists";
-        public static string ERROR_NAME_DOES_NOT_EXIST = "Error: Name does not exist";
-        public static string ERROR_NAME_CANNOT_BE_EMPTY = "Error: Name cannot be blank, empty or null";
-        public static string ERROR_NAME_TOO_LONG = "Error: Name cannot be longer than 75 characters";
-        public static string ERROR_ADDRESS_CANNOT_BE_EMPTY = "Error: Address cannot be empty";
-        public static string ERROR_ADDRESS_TOO_LONG = "Error: Address cannot be longer than 150 characters";
+        public const string ERROR_NAME_EXISTS = "Error: Name already exists";
+        public const string ERROR_NAME_DOES_NOT_EXIST = "Error: Name does not exist";
+        public const string ERROR_NAME_CANNOT_BE_EMPTY = "Error: Name cannot be blank, empty or null";
+        public const string ERROR_NAME_TOO_LONG = "Error: Name cannot be longer than 75 characters";
+        public const string ERROR_ADDRESS_CANNOT_BE_EMPTY = "Error: Address cannot be empty";
+        public const string ERROR_ADDRESS_TOO_LONG = "Error: Address cannot be longer than 150 characters";
     }
 }
